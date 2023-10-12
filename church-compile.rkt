449ab268569813322ecfb51f2faeabab08df22fe
Reza <rezahandzalah@gmail.com>
2023-10-12 19:56:07 +0700

    wip

--- church-compile.rkt
+++ church-compile.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require racket/hash "definitions.rkt")
+(require "definitions.rkt")
 ;; Assignment 4: A church-compiler for Scheme, to Lambda-calculus
 
 (provide (all-defined-out))
@@ -60,9 +60,6 @@
 ; Example usage: (check-true ((church->bool test) '()))
 (define (church->bool c-bool)
   ((c-bool (lambda (_) #t)) (lambda (_) #f)))
-(define (church->boolean b)
-  ; ((church->bool b) '()))
-  ((church->bool b) #f))
 
 ; A church-encoded cons-cell is a function taking a when-cons callback, and a when-null callback (thunk),
 ;   returning when-cons applied on the car and cdr elements
@@ -77,159 +74,6 @@
 
 ;; Write your church-compiling code below:
 
-; logs:
-; only ll default arm calls lookup
-; churchify and ll shouldn't overlap in terminal arms
-; nesting lambda means growing fargs
-(define (lookup op bind-map)
-  (cond [(procedure? op)  op]
-        [else             (hash-ref bind-map op #f)]))
-
-(define/contract (bind-map-new binds)
-  (-> list? hash?)
-  (define (f kv)
-    (define e (second kv))
-    (cons (first kv) (churchify e)))
-
-  (make-immutable-hash (map f binds)))
-
-(define/contract (bind-map-set binds outer)
-  (-> list? hash? hash?)
-  (define (f kv)
-    (define e (second kv))
-    (define v (cond [(number? e)                  (cnat e)]
-                    [(not (hash-ref outer e #f))  (ll e outer)]
-                    [else                         (hash-ref outer e)]))
-
-    (if (procedure? v)
-        (void)
-        (raise-result-error (string->symbol "church-encoded type") "procedure?" v))
-
-    (cons (first kv) v))
-
-  (define bm (make-immutable-hash (map f binds)))
-  (hash-union outer
-              bm
-              #:combine/key (lambda (_k _v1 v2) v2)))
-
-(define/contract (bind-map-set* binds outer)
-  (-> list? hash? hash?)
-  (define (f* bind acc)
-    (define e (second bind))
-    (define v (cond [(number? e)                  (cnat e)]
-                    [(member e (hash-keys acc))   (hash-ref acc e)]
-                    [(not (hash-ref outer e #f))  (ll e acc)]
-                    [else                         (hash-ref outer e)]))
-    (hash-set acc (first bind) v))
-
-  (define bm (foldl f* outer binds))
-  (hash-union outer
-              bm
-              #:combine/key (lambda (_k _v1 v2) v2)))
-
-(define/contract (ll body bind-map)
-  (-> any? any? procedure?)
-  (define (val e) (ll e bind-map))
-  (match body
-    ; [(? literal? body)  (churchify-terminal body)]
-    [(? literal? body)  (churchify body)]
-
-    [`(if ,b ,then ,els)
-       ((((ll b bind-map) (lambda () (ll then bind-map))) (lambda () (ll els bind-map))))]
-
-    [`(,(? binary? op) ,a1 ,a2)
-      ((lambda (fn) (fn (val op) (val a1) (val a2)))
-       (lambda (op a1 a2) ((op a1) a2)))]
-
-    [`(,(? unary? op) ,arg)
-      ((lambda (fn) (fn (val op) (val arg)))
-       (lambda (op arg) (op arg)))]
-
-    [`(lambda (,fargs ...) ,body/n)
-     (begin
-       ; Fill body with current bind-map, leaving body only with its formal args.
-       ; Implicit: use ll's bind-map.
-       (define/contract (fill-free-vars e fargs)
-         (-> any? any? any?)
-         (begin
-           (define (ffv e) (fill-free-vars e fargs))
-           (match e
-             [`(lambda (,fargs/n ...) ,body/n/n)
-              (begin
-                `(lambda ,fargs/n ,(fill-free-vars body/n/n (append fargs fargs/n))))]
-
-             [`(if ,b ,then ,els)
-              (begin
-                ; We can reduce right here if none is member of formal args.
-                (define filled (list (ffv b) (ffv then) (ffv els)))
-
-                (define none-is-fargs (andmap (lambda (var) (not (member var fargs))) filled))
-                (cond [none-is-fargs
-                        (begin
-                          (((first filled) (second filled)) (third filled)))]
-                      [else
-                        (begin
-                          (define (try-church->boolean)
-                            (define filled-b (first filled))
-                            (cond
-                              [(member (object-name filled-b) (list 'TRUE 'FALSE))  (church->boolean filled-b)]
-                              [else                                                 filled-b]))
-
-                          (define new-if `(if ,(try-church->boolean) ,(second filled) ,(third filled)))
-                          new-if)]))]
-
-             [`(,(? binary? op) ,a1 ,a2)
-              (begin
-                ; We can reduce right here if none is member of formal args.
-                (define filled (list (ffv op)(ffv a1)(ffv a2)))
-
-                (define none-is-fargs (andmap (lambda (var) (not (member var fargs))) filled))
-
-                FALSE
-
-                )]
-
-             [`(,op ,arg)  ; unary? or user defined lambda
-              (begin
-                (define filled `(,(ffv op) ,(ffv arg)))
-
-                ; defer: ffv with optional arg none-is-fargs
-
-                ; We can reduce right here if none is member of formal args.
-                (define none-is-fargs (andmap (lambda (var) (not (member var fargs))) filled))
-                (cond [none-is-fargs  ((first filled) (second filled))]
-                      [else           filled]))]
-
-             [var
-               (cond [(member var fargs)  var]
-                     [else                (ll var bind-map)])])))
-
-       ; (evalnew `(lambda ,fargs ,(fill-free-vars body/n fargs))))]
-       ; unpack and replace
-       ; (ll ?? bind-map)
-       (define (bod)
-           (define ret (fill-free-vars body/n fargs))
-           (fill-free-vars body/n fargs)
-)
-       (evalnew `(lambda ,fargs ,(bod)))
-       )]
-
-    [`(,op ,arg)
-      ((ll op bind-map) (ll arg bind-map))
-      ; `(,(churchify op) ,(churchify arg))  ; goal
-      ]
-
-    [`(letrec ,binds/n ,e)
-     'todo]
-
-    [`(let ,binds/n ,e)
-      (ll e (bind-map-set binds/n bind-map))]
-
-    [`(let* ,binds/n ,e)
-      (ll e (bind-map-set* binds/n bind-map))]
-
-    [v  (lookup v bind-map)]))
-
 (define (churchify-terminal l)
   (match l
     [(? number? l)  (cnat l)]
@@ -237,68 +81,14 @@
     ['#f            FALSE]
     [`'()           NIL]))
 
-; bak
-; (define (churchify e)
-;   (define (left-left bind acc)
-;     (match bind
-;       ['()           acc]
-;       [`(,x . (,e))  `((lambda (,x) ,acc) ,(churchify e))]))
-;
-;   (match e
-;     [`(net* ,binds ,e-b)
-;       (foldr left-left (churchify e-b) binds)]
-;
-;     [`(net ,binds ,e-b)
-;       (foldl left-left (churchify e-b) binds)]
-;
-;     [(? literal? e)    (churchify-terminal e)]
-;     [`(let ,binds ,e)  (ll e (bind-map-new binds))]
-;
-;     ; ; idea: fold over binds first, then on body
-;     ; [`(net* ,binds ,e-b)
-;     ;   (define (left-left bind acc)
-;     ;     (match bind
-;     ;       ['()           acc]
-;     ;       [`(,x . (,e))  `((lambda (,x) ,(churchify e)) ,acc)]))
-;     ;   (define folded (foldl left-left (lambda (x) x) binds))
-;     ;   (folded (churchify e-b))]
-;
-;     [`(if ,e0 ,e1 ,e2)
-;       (churchify `(,e0 (lambda () ,e1) (lambda () ,e2)))]
-;
-;     [`(lambda ,xs ,e-body)
-;       (define (h xs)
-;         (match xs
-;           ['()           (churchify e-body)]
-;           [`(,x . ,rst)  `(lambda (,x) ,(h rst))]))
-;       (h xs)]
-;
-;     [`(,binary-op ,e1 ,e2)
-;       `((,(churchify binary-op) ,(churchify e1)) ,(churchify e2))]
-;       ; ((f 1) 2)
-;
-;     [`(,op ,arg)
-;       `(,(churchify op) ,(churchify arg))]
-;       ; (f 1)
-;
-;     [(not (? list? e))  e]
-;
-;     [(? list es)
-;       (define (surround es)
-;         (match es
-;           [`(,fst ,snd . ,rst)  (surround `((,(churchify fst) ,(churchify snd)) ,@rst))]
-;           [es                   (first es)])) ; simple guy, I see pair of extraneous parens, I apply first. probably empty lists etc etc
-;       (surround es)]
-;
-;     [_ e]
-;
-;     ))
-
+; churchify recursively walks the AST and converts each expression in the input language (defined above)
+;   to an equivalent (when converted back via each church->XYZ) expression in the output language (defined above)
 (define (churchify e)
   (define (left-left bind acc)
     (match bind
       ['()           acc]
-      [`(,x . (,e))  `((lambda (,x) ,acc) ,(churchify e))]))
+      [`(,x . (,e))
+        `((lambda (,x) ,acc)  ,(churchify e))]))
 
   (match e
     [`(let* ,binds ,e-b)
@@ -310,7 +100,7 @@
     [(? literal? e)    (churchify-terminal e)]
 
     [`(if ,e0 ,e1 ,e2)
-      (churchify `(,e0 (lambda () ,e1) (lambda () ,e2)))]
+      `(,(churchify `(,(churchify e0) (lambda () ,(churchify e1)) (lambda () ,(churchify e2)))))]
 
     [`(lambda ,xs ,e-body)
       (define (h xs)
@@ -320,9 +110,15 @@
       (h xs)]
 
     [`(,binary-op ,e1 ,e2)
-      `((,(churchify binary-op) ,(churchify e1)) ,(churchify e2))]
+      (display "inp:\t")(displayln e)
+      (define bp
+        `((,binary-op ,e1) ,e2)
+      )
+      (display "out:\t")(displayln bp)
+      bp]
 
     [`(,op ,arg)
+      (display "oa inp:\t")(displayln e)
       `(,(churchify op) ,(churchify arg))]
 
     [(not (? list? _))  e]
@@ -334,36 +130,8 @@
           [es                   (first es)])) ; simple guy, I see pair of extraneous parens, I apply first. probably empty lists etc etc
       (surround es)]))
 
-; churchify recursively walks the AST and converts each expression in the input language (defined above)
-;   to an equivalent (when converted back via each church->XYZ) expression in the output language (defined above)
-
 ; Takes a whole program in the input language, and converts it into an equivalent program in lambda-calc
 ; Build a let expression containing all helpers and the input program.
-; Delegate the expression to churchify.
-; (define (church-compile program)
-;   ; Define primitive operations and needed helpers using a top-level let form?
-;   (churchify
-;    `(let (
-;           [add1 ,SUCC]
-;           [succ ,SUCC]
-;           [null? ,NULL?]
-;           [nil? ,NULL?]
-;           [sub1 ,PRED]
-;           [cons ,CONS]
-;           [not ,NOT]
-;           [and ,AND]
-;           [car ,CAR]
-;           [cdr ,CDR]
-;           [= ,EQ?]
-;           [+ ,PLUS]
-;
-;           [nol? ,ZERO?]
-;           [zero? ,ZERO?]
-;           [- ,MINUS]
-;           [* ,MUL]
-;           )
-;       ,program)))
-
 (define (church-compile program)
   (evalnew (churchify
     `(let ([add1 ,SUCC] [null? ,NULL?] [sub1 ,PRED] [cons ,CONS] [not ,NOT] [and ,AND] [car ,CAR]
